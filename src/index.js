1e0227455b3fc809a15a0d619d5f634894a4b304
Mihndim Julius <mihndim2016@gmail.com>
2021-07-13 19:26:23 -0400

    Add Task and TodoList classes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+class Task {
+  constructor(description, completed, index){
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
 }
 
-document.body.appendChild(component());
+class ToDoList {
+  constructor(){
+    let tasks = [];
+    this.tasks = tasks;
+  }
+
+  addTasksToList = (description, completed, index) => {
+    this.tasks.push(new Task(description, completed, index));
+
+    const tasksObjects = JSON.stringify(this.tasks);
+    localStorage.setItem('tasks', tasksObjects);
+  }
+
+}
+
+
+
+ 
