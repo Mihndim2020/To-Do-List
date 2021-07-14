b774a678262f00c675d9199966d2a0fe28843c8d
Mihndim Julius <mihndim2016@gmail.com>
2021-07-13 21:16:05 -0400

    Add css and javascript to create interface

--- src/index.js
+++ src/index.js
@@ -1,29 +1,88 @@
 import _ from 'lodash';
 import './style.css';
 
-class Task {
-  constructor(description, completed, index){
-    this.description = description;
-    this.completed = completed;
-    this.index = index;
-  }
-}
+const tasks = [
+  { description: 'Laundary', completed: true, index: 1 },
+  { description: 'Car wash', completed: false, index: 2 },
+  { description: 'Swimming', completed: false, index: 3 },
+];
 
-class ToDoList {
-  constructor(){
-    let tasks = [];
-    this.tasks = tasks;
-  }
 
-  addTasksToList = (description, completed, index) => {
-    this.tasks.push(new Task(description, completed, index));
+const toDolist = () => {
+  const title = () => {
+    const li = document.createElement('li');
+    li.id = 'list-title';
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Today\'s To Do';
+    const i = document.createElement('i');
+    i.classList.add('fas', 'fa-sync-alt');     
+    i.id = 'refresh-icon';
 
-    const tasksObjects = JSON.stringify(this.tasks);
-    localStorage.setItem('tasks', tasksObjects);
-  }
+    li.appendChild(h2);
+    li.appendChild(i);
 
-}
+    return li;
+  };
 
+  const addTask = () => {
+    const li = document.createElement('li');
+    li.id = 'new-tasks';
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Add to your list...';
+    input.id = 'list-item';
+
+    li.appendChild(input);
+
+    return li;
+  };
+
+  const taskList = (task) => {
+    const li = document.createElement('li');
+
+    const div = document.createElement('div');
+
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.name = 'completed';
+
+    const p = document.createElement('p');
+    p.textContent = task.description;
+
+    div.appendChild(input);
+    div.appendChild(p);
+
+    li.appendChild(div);
+
+    const i = document.createElement('i');
+    i.classList.add('fas', 'fa-trash-alt');
+
+    li.appendChild(i);
+
+    return li;
+  };
+  const completed = () => {
+    const li = document.createElement('li');
+
+    li.textContent = 'Clear all completed';
+    li.id = 'clear';
+
+    return li;
+  };
+
+  const ul = document.querySelector('ul');
+
+  ul.appendChild(title());
+  ul.appendChild(addTask());
+
+  tasks.sort((a, b) => ((a.index > b.index) ? 1 : -1));
+  tasks.forEach((task) => ul.appendChild(taskList(task)));
+
+  ul.appendChild(completed());
+};
+
+toDolist();
 
 
  
