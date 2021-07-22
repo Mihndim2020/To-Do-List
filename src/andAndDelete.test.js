5a6bd991bad38a203a7c9afbb9207cb39b8cd95b
Mihndim Julius <mihndim2016@gmail.com>
2021-07-22 15:48:57 -0400

    It creates a list with corresponding inputs

---
+++ src/andAndDelete.test.js
@@ -0,0 +1,74 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {tasks, addTasks, createNewTask, repopulateList } from './backEnd';
+import { taskList } from './index.js';
+
+describe('It adds and deletes items from the list', () => {
+
+  const task1 = {
+    description: 'Car wash',
+    complemented: false,
+    index: 0,
+  };
+  const task2 = {
+    description: 'Laundary',
+    complemented: false,
+    index: 1,
+  };
+
+  const taskDescription = 'Pain the house';
+
+  const ul = document.createElement('ul');
+
+  test('it creates a list corresponding inputs', () => {
+
+    createNewTask(taskDescription);
+
+    expect(tasks[tasks.length - 1].description).toBe(taskDescription);
+
+    ul.appendChild(taskList(tasks[tasks.length - 1]));
+
+    expect(ul.innerHTML.includes(taskDescription)).toBe(true);
+  });
+
+  test('it adds and deletes an item from the To Do List', () => {
+    const addedTask1 = addTasks(task1.description, task1.completed, task1.index);
+    const addedTask2 = addTasks(task2.description, task2.completed, task2.index);
+
+    ul.innerHTML = '';
+    const addTaskToDOM = (task) => {
+    
+      const listElement = document.createElement('li');
+
+      listElement.classList.add('draggable');
+      listElement.setAttribute('task', task.index);
+
+      const input = document.createElement('input');
+      input.classList.add('completed');
+      input.setAttribute('checked', (task.completed ? 'true' : 'false'));
+
+      const p = document.createElement('p');
+      p.classList.add('description');
+      p.textContent = task.description;
+
+      listElement.appendChild(input);
+      listElement.appendChild(p);
+      ul.appendChild(listElement);
+    };
+
+    addTaskToDOM(addedTask1);
+    addTaskToDOM(addedTask2);
+
+    document.body.appendChild(ul);
+
+    ul.removeChild(ul.childNodes[0]);
+
+    repopulateList();
+
+    expect(tasks.length).toBe(1);
+    expect(tasks[0].description).toBe(addedTask2.description);
+  });
+
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
-import './style.css';
+// import './style.css';
 import {
   loadTaskList, tasks, taskCompleteUpdate, createNewTask, editDescription, repopulateList,
 } from './backEnd';
 import {
   dragstart, dragover, dragleave, drop, dragend,
 } from './dragAndDrop';
+
+const taskList = (task) => {
+  const li = document.createElement('li');
+  li.classList.add('draggable');
+  li.setAttribute('task', task.index);
+  li.draggable = true;
+
+  const ul = document.getElementsByTagName('ul');
+
+  const div = document.createElement('div');
+
+  const input = document.createElement('input');
+  input.classList.add('completed');
+  input.type = 'checkbox';
+  input.name = 'completed';
+  input.addEventListener('click', () => taskCompleteUpdate(parseInt(li.getAttribute('task'), 10), input.checked));
+
+  const p = document.createElement('p');
+  p.classList.add('description');
+  p.contentEditable = 'true';
+  p.textContent = task.description;
+  p.addEventListener('input', () => editDescription(parseInt(li.getAttribute('task'), 10), p.textContent));
+
+  div.appendChild(input);
+  div.appendChild(p);
+
+  li.appendChild(div);
+
+  const i = document.createElement('i');
+  i.classList.add('fas', 'fa-trash-alt');
+  i.addEventListener('click', () => {
+    ul.removeChild(li);
+    localStorage.clear();
+
+    repopulateList();
+  });
+
+  li.addEventListener('dragstart', () => dragstart(li));
+  li.addEventListener('dragover', (e) => dragover(li, e));
+  li.addEventListener('dragleave', () => dragleave(li));
+  li.addEventListener('drop', () => {
+    drop(li);
+  });
+  li.addEventListener('dragend', () => {
+    dragend(li);
+  });
+
+  li.appendChild(i);
+
+  return li;
+};
 
 const toDolist = () => {
   const ul = document.querySelector('ul');
@@ -22,56 +73,7 @@ const toDolist = () => {
     li.appendChild(i);
 
     return li;
-  };
-
-  const taskList = (task) => {
-    const li = document.createElement('li');
-    li.classList.add('draggable');
-    li.setAttribute('task', task.index);
-    li.draggable = true;
-
-    const div = document.createElement('div');
-
-    const input = document.createElement('input');
-    input.classList.add('completed');
-    input.type = 'checkbox';
-    input.name = 'completed';
-    input.addEventListener('click', () => taskCompleteUpdate(parseInt(li.getAttribute('task'), 10), input.checked));
-
-    const p = document.createElement('p');
-    p.classList.add('description');
-    p.contentEditable = 'true';
-    p.textContent = task.description;
-    p.addEventListener('input', () => editDescription(parseInt(li.getAttribute('task'), 10), p.textContent));
-
-    div.appendChild(input);
-    div.appendChild(p);
-
-    li.appendChild(div);
-
-    const i = document.createElement('i');
-    i.classList.add('fas', 'fa-trash-alt');
-    i.addEventListener('click', () => {
-      ul.removeChild(li);
-      localStorage.clear();
-
-      repopulateList();
-    });
-
-    li.addEventListener('dragstart', () => dragstart(li));
-    li.addEventListener('dragover', (e) => dragover(li, e));
-    li.addEventListener('dragleave', () => dragleave(li));
-    li.addEventListener('drop', () => {
-      drop(li);
-    });
-    li.addEventListener('dragend', () => {
-      dragend(li);
-    });
-
-    li.appendChild(i);
-
-    return li;
-  };
+  }; 
 
   const addTaskInput = () => {
     const li = document.createElement('li');
@@ -123,13 +125,16 @@ const toDolist = () => {
     return li;
   };
 
-  ul.appendChild(title());
-  ul.appendChild(addTaskInput());
+  // ul.appendChild(title());
+  // ul.appendChild(addTaskInput());
 
   tasks.sort((a, b) => ((a.index > b.index) ? 1 : -1));
   tasks.forEach((task) => ul.appendChild(taskList(task)));
 
-  ul.appendChild(completed());
+  // ul.appendChild(completed());
 };
 
 toDolist(loadTaskList());
+
+export { toDolist, taskList };
+
